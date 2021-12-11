4fce7daac1637931e766c2b26480bd3c2cab8216
wyt1215819315 <2676831402@qq.com>
2021-12-11 15:23:09 +0800

    update

--- genshin_cookie.sql
+++ genshin_cookie.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地数据库
+ Source Server         : ai开发数据库
  Source Server Type    : MySQL
  Source Server Version : 80024
  Source Host           : 192.168.123.165:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80024
  File Encoding         : 65001
 
- Date: 09/12/2021 12:43:21
+ Date: 11/12/2021 14:50:27
 */
 
 SET NAMES utf8mb4;
@@ -22,61 +22,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `genshin_cookie`;
 CREATE TABLE `genshin_cookie`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `userid` bigint NULL DEFAULT NULL,
-  `record_id` int NULL DEFAULT NULL,
-  `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `cookie` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `server` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'cn_gf01',
-  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `uid`(`uid`) USING BTREE,
-  INDEX `cookie`(`cookie`) USING BTREE,
-  INDEX `record_id`(`record_id`) USING BTREE,
-  UNIQUE INDEX `userid`(`userid`) USING BTREE,
-  UNIQUE INDEX `INDEX_UNIQUE_userid`(`userid`) USING BTREE,
-  CONSTRAINT `genshin_cookie_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `system_record` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `genshin_cookie_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `sys_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1467376931699888132 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for sys_role
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for sys_role_user
--- ----------------------------
-DROP TABLE IF EXISTS `sys_role_user`;
-CREATE TABLE `sys_role_user`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `sys_user_id` bigint NULL DEFAULT NULL,
-  `sys_role_id` int NULL DEFAULT NULL,
+  `record_id` int NULL DEFAULT NULL,
+  `uid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `cookie` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `server` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'cn_gf01',
+  `other` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `sys_user_id`(`sys_user_id`) USING BTREE,
-  INDEX `sys_role_id`(`sys_role_id`) USING BTREE,
-  CONSTRAINT `sys_role_user_ibfk_2` FOREIGN KEY (`sys_role_id`) REFERENCES `sys_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `sys_role_user_ibfk_3` FOREIGN KEY (`sys_user_id`) REFERENCES `sys_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Table structure for sys_user
--- ----------------------------
-DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `regdate` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `username`(`username`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+  UNIQUE INDEX `uid`(`uid`) USING BTREE,
+  UNIQUE INDEX `cookie`(`cookie`) USING BTREE,
+  INDEX `record_id`(`record_id`) USING BTREE,
+  CONSTRAINT `genshin_cookie_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `system_record` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for system_config
@@ -84,11 +41,12 @@ CREATE TABLE `sys_user`  (
 DROP TABLE IF EXISTS `system_config`;
 CREATE TABLE `system_config`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `value` varchar(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `bond` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `other` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `INDEX_UNIQUE_bond`(`bond`) USING BTREE
+  UNIQUE INDEX `key`(`key`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -102,11 +60,11 @@ CREATE TABLE `system_log`  (
   `send_text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   `send_whole` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
   `userId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `date` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   `other` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1468732785896427522 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 225 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for system_record
@@ -114,15 +72,31 @@ CREATE TABLE `system_log`  (
 DROP TABLE IF EXISTS `system_record`;
 CREATE TABLE `system_record`  (
   `id` int NOT NULL AUTO_INCREMENT,
-  `xiaomi_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `aligenre_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `add_date` datetime NULL DEFAULT CURRENT_TIMESTAMP,
+  `xiaomi_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '小爱的唯一标识',
+  `aligenre_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '天猫精灵的唯一标识',
+  `add_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '字段创建时间',
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `other` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `xiaomi_id`(`xiaomi_id`) USING BTREE,
-  UNIQUE INDEX `aligenre_id`(`aligenre_id`) USING BTREE,
-  UNIQUE INDEX `INDEX_UNIQUE_xiaomi_id`(`xiaomi_id`) USING BTREE,
-  UNIQUE INDEX `INDEX_UNIQUE_aligenre_id`(`aligenre_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 181 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `aligenre_id`(`aligenre_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 239 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for u_turnoff_computer
+-- ----------------------------
+DROP TABLE IF EXISTS `u_turnoff_computer`;
+CREATE TABLE `u_turnoff_computer`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `record_id` int NOT NULL,
+  `secert_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `other` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `secert_key`(`secert_key`) USING BTREE,
+  INDEX `record_id`(`record_id`) USING BTREE,
+  CONSTRAINT `u_turnoff_computer_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `system_record` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- src/main/java/com/oldwu/controller/AligenieController.java
+++ src/main/java/com/oldwu/controller/AligenieController.java
@@ -12,16 +12,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
-@RequestMapping("/aligenre")
-public class AligenreController {
-    private final Log logger = LogFactory.getLog(AligenreController.class);
+@RequestMapping("/aligenie")
+public class AligenieController {
+    private final Log logger = LogFactory.getLog(AligenieController.class);
     @Autowired
     private BaseService baseService;
 
